77969e8c316fe7afc8b807436f9bcc7bd4b537cc
si5458415 <129784333+si5458415@users.noreply.github.com>
2023-04-18 18:01:20 +0300

    Create GalaxyViewer .py

---
+++ GalaxyViewer .py
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // Здесь код программы для просмотра галактик и звездных скоплений
+    return 0;
+}
